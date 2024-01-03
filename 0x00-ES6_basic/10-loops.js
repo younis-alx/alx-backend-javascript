f7b64f87907c464304b8d9abcf63848e8385c222
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-01-03 05:30:37 +0000

    eslint style fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-  
-    return newArray;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}
