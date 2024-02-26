8462877ff2612dfa97de259d71106a03b127eee0
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-02-26 06:37:13 +0000

    update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,8 +4,7 @@
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
-  module.exports = displayMessage;
-  
+  console.log(msg);
+};
+
+module.exports = displayMessage;
