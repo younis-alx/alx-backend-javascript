4b32cc7cbe286419f0d8eb6ded72c807697d0725
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-02-28 06:08:01 +0000

    test: create 6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI(success), where success == true', () => new Promise((done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      });
+  }));
+});
